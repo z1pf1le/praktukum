eacbc425b2b44606579ebe392d6453bf240a6c8e
z1pf1le <total.zip.file@gmail.com>
2023-11-20 20:28:52 +0300

    Initial commit

---
+++ src/DecimalToHex.java
@@ -0,0 +1,10 @@
+public class DecimalToHex {
+
+
+    public String calculate(int arg) {
+        int decimalNumber = arg;
+        String hexadecimalNumber = Integer.toHexString(decimalNumber);
+        System.out.println("Шестнадцатеричное представление: " + hexadecimalNumber);
+        return hexadecimalNumber;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main{
+    public static void main(String[] args) {
+        DecimalToHex dec = new DecimalToHex();
+        dec.calculate(35465);
+    }
+}
