4badb098061d49af967b2f6d1cf5ecf78c64c437
z1pf1le <total.zip.file@gmail.com>
2023-11-23 21:05:42 +0300

    array sort

---
+++ src/four/BubbleSort.java
@@ -0,0 +1,30 @@
+//package four;
+//
+//public class BubbleSort {
+//    public static void bs(array ar) {
+//
+//
+//        // Сортировка пузырьком
+//        int n = array.length;
+//        for (int i = 0; i < n-1; i++) {
+//            for (int j = 0; j < n-i-1; j++) {
+//                if (array[j] > array[j+1]) {
+//                    // обмен значениями
+//                    int temp = array[j];
+//                    array[j] = array[j+1];
+//                    array[j+1] = temp;
+//                }
+//            }
+//        }
+//
+//        System.out.println("отсортированный массив по возрастанию:");
+//        printArray(array);
+//    }
+//
+//    // Вспомогательный метод для вывода массива
+//    public static void printArray(int[] arr) {
+//        for (int num : arr) {
+//            System.out.print(num + " ");
+//        }
+//    }
+//}
--- src/four/RandomArrayGenerator.java
+++ src/four/RandomArrayGenerator.java
@@ -1,10 +1,11 @@
 package four;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class RandomArrayGenerator {
     public static void main(String[] args) {
-        final int K = 5; // установка значения K
+        final int K = 50; // установка значения K
         final int ARRAY_SIZE = 100; // размер массива
 
         int[] randomArray = new int[ARRAY_SIZE];
@@ -16,18 +17,29 @@ public class RandomArrayGenerator {
         }
 
         // Вывод сгенерированного массива
-        System.out.println(randomArray);
+//        System.out.println(randomArray);
         int count = 0;
+        int count2 = 0;
         System.out.println("Сгенерированный массив из " + ARRAY_SIZE + " чисел на интервале [0:" + K + "]:");
         for (int num : randomArray) {
 
             System.out.print(num + " ");
-//            System.out.println(PrimeNumberChecker.isPrime(num));
             count = count + PrimeNumberChecker.isPrime(num);
+
+            if (num / 3 == 0){
+                count2++;
+            }
         }
 
-        System.out.println("\nпростых: ");
+        System.out.println("\nпростых чисел:");
         System.out.println(count);
+        System.out.println("\nчисел кратных 3:");
+        System.out.println(count2);
+        Arrays.sort(randomArray);
+
+        for (int num: randomArray){
+            System.out.print(num + " ");
+        }
     }
 
 }
