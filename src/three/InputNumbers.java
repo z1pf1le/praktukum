a5ddf1fa67264c732cb70ab648e923700650de03
z1pf1le <total.zip.file@gmail.com>
2023-11-23 18:07:49 +0300

    третья начало

---
+++ src/three/InputNumbers.java
@@ -0,0 +1,22 @@
+package three;
+
+import java.util.Scanner;
+
+public class InputNumbers{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите первое число: ");
+        int firstNumber = scanner.nextInt();
+
+
+        System.out.print("Введите второе число: ");
+        int secondNumber = scanner.nextInt();
+
+        System.out.print("Введите третее число: ");
+        int thirdNumber = scanner.nextInt();
+
+        System.out.println(Integer.toString(firstNumber) + " " + Integer.toString(secondNumber));
+
+    }
+}
