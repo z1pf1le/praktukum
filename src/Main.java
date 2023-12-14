a011d2dc1a24d8c759d226bc59f5d18c9dce1456
z1pf1le <total.zip.file@gmail.com>
2023-12-14 16:51:34 +0300

    array sort

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main{
-//    public static void main(String[] args) {
-//        DecimalToHex dec = new DecimalToHex();
-//        System.out.println(dec.calculate(67));
-//        dec.calculate(35465);
-//    }
-}
